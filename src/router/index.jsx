89fe510ced72433bd4492e4f211c41f022c1ef30
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2024-10-15 14:23:29 +0800

    Updates

--- src/router/index.jsx
+++
@@ -1,7 +0,0 @@
-import { createHashRouter } from 'react-router-dom';
-
-import routes from './routes';
-
-const router = createHashRouter(routes);
-
-export default router;
--- src/views/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div>
-      <h1>六角學院課程範例（React 直播班）</h1>
-    </div>
-  );
-}
-
-export default Home;
