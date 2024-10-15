21f51ed4ddaf130edb0ea6aa41bb4489e64b66a8
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2024-10-15 13:56:31 +0800

    feat: 🎸 vite.config.js
    優化 Vite 設定檔案，只需要輸入環境變數即可部署專案到對應的 gh-page

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
@@ -5,7 +7,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/live-react-dashboard-2024/' : '/',
+  // 可參考：https://israynotarray.com/nodejs/20230214/796256725/
+  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORIES}/` : '/',
   plugins: [react()],
   resolve: {
     alias: {
